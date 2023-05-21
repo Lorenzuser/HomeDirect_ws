8ced95331da5ea6c840822228179d4bf62e4db09
Al3xAda <alexander.janzen15@outlook.de>
2023-05-21 14:13:25 +0200

    update from lokal ros2_ws directory

--- src/tb3_rfid/tb3_rfid/publish_taginfo_node.py
+++ src/tb3_rfid/tb3_rfid/publish_taginfo_node.py
@@ -3,13 +3,13 @@
 # This is a program to publish 'id' and 'text' which were perviously written to the RFID tag 
 # (see ~/Projekt--HomeDirect/RFID/Write.py)
 
+# import necessary packages & co.
 import rclpy
 from rclpy.node import Node
 # thes following two lines are from https://pimylifeup.com/raspberry-pi-rfid-rc522/ 
 # (site also referenced elsewhere in this project)
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
-# Allows the use of the String message type
 from std_msgs.msg import String
 
 # following code orientates itself acording ~/Projekt--HomeDirect/RFID/Read.py 
@@ -19,15 +19,13 @@ from std_msgs.msg import String
 class TagInfoNode(Node):
 
     def __init__(self):
-        # call super() and pass the name of the node
-        super().__init__("rfid_reader")
+        super().__init__("rfid_reader") # node name might better be changed
         # Create Publisher 
-        self.tag_info_pub_ = self.create_publisher(
+        self.publisher_ = self.create_publisher(
             String, "tag_info", 10) # (message type, topic name, queue size)
         # Create timer
         self.timer_ = self.create_timer(
-            # Read and publish tag info every second
-            1.0, self.publish_tag_info)
+            1.0, self.publish_tag_info) # Read and publish tag info every second
         # reader = SimpleMFRC522()
         # Let them know node is running
         self.get_logger().info("Read tag info node has been started")   
@@ -37,24 +35,22 @@ class TagInfoNode(Node):
         # define 'reader' , this will be used to read the tag info
         reader = SimpleMFRC522()
 
-        # Programm-Elements from ~/RFID/Read.py ~/RFID/AiPaste.py 
-        # 'try' statement used to catch errors and clean up propperly
+        # referencing: ~/RFID/Read.py ~/RFID/AiPaste.py 
+        # 'try' + 'finnaly' in order to: catch errors and clean up propperly
         try:
-            # write the tag info to 'id' and 'text'
+            # define 'id' and 'text' 
             id, text = reader.read()
-            # create message by calling the String message type
+            # create message by calling 'String' message type
             msg = String()
-            # fill the message with the tag info
+            # fill message 
             msg.data = "ID: " + str(id) + " Text: " + text
-            # publish the message
-            self.tag_info_pub_.publish(msg)
-            # print the tag info to the terminal
+            # publish message
+            self.publisher_.publish(msg)
+            # print tag info 
             self.get_logger().info('Tag ID: %s' % id)
-            # Let them know the tag info was published
+            # print Node Status
             self.get_logger().info("Tag info published")
 
-        # handle exiting the program
-        # I am not sure if this should go here or in the main function
         finally:
             # clean up GPIO pins
             GPIO.cleanup()
