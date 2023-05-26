fa126ffd3bf70df20d4b96cf8c208eb93d4378b3
Al3xAda <alexander.janzen15@outlook.de>
2023-05-26 20:19:59 +0200

    firts draft rfid client

---
+++ src/tb3_rfid/tb3_rfid/goal_determiner_client.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3#
+
+# Subscribes to the 'tag_info' topic
+# and helps the Navigation-Node to determine the goal(right Room) by providing the patient's tag info
+
+import sys
+
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+class TagInfoClient(Node):
+
+    # Create Subscriber and Client
+    def __init__(self):
+        super().__init__('tag_info_client') 
+        self.subscription = self.create_subscription(
+            String, # msg type
+            'tag_info', # topic
+            self.taginfo_callback, # call for the programm to make client requests
+            10) # queue size
+        self.client = self.create_client(String, 'tag_info_service') # client is called 'tag_info_service'
+        while not self.client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('Service not available, waiting again...')
+        self.req = String.Request() # String: msg type
+        # Log
+        self.get_logger().info("Taginfo subscriber has been started")    
+
+    # Handle received tag info
+    def taginfo_callback(self, msg):
+            self.get_logger().info('Received tag info: "%s"' % msg.data)
+            self.req.data = msg.data
+            self.future = self.client.call_async(self.req)
+            self.future.add_done_callback(self.taginfo_callback) # Log-Feedback callback
+
+    # Log-Feedback
+    def taginfo_callback(self, future):
+        try:
+            response = future.result()
+            if response.success:
+                self.get_logger().info('Tag request has been sent')
+            else:
+                self.get_logger().info('Server failed to process the request')
+        except Exception as e:
+            self.get_logger().info(f'Tag request failed: {e}')
+
+def main(args=None):
+    rclpy.init(args=args)
+    taginfo_client = TagInfoClient()
+    rclpy.spin(taginfo_client)
+    taginfo_client.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
+                
+
+
+        
+
+
