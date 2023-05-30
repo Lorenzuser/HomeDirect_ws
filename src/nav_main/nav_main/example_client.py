d02198ae17ce3d3fcc81aadaf3db2117ac205ef7
Al3xAda <alexander.janzen15@outlook.de>
2023-05-30 12:50:28 +0200

    new rfid class, set x,y and request in callback

--- src/nav_main/nav_main/example_client.py
+++ src/nav_main/nav_main/example_client.py
@@ -5,13 +5,42 @@ import json
 from example_interfaces.srv import AddTwoInts
 import rclpy
 from rclpy.node import Node
+from std_msgs.msg import String
 
 FILENAME = '/home/dev/HomeDirect_ws/config/' + str(sys.argv[2]) + '.json'
 # Daten aus der Datei lesen
 def read_from_file():
     with open(FILENAME, 'r') as openfile:
         return json.load(openfile)
-    
+
+class NameTagSubscriber(Node):
+
+    # Subscribes to the 'taginfo' topic
+    def __init__(self):
+        super().__init__('name_tag_subscriber')
+        self.subscription = self.create_subscription(
+            String, # msg type
+            'tag_info', # topic
+            self.match_goal_callback, # reference "output" function
+            10) # queue size
+        self.subscription  # prevent unused variable warning
+        self.minimal_client = MinimalClientAsync()
+        self.get_logger().info("Taginfo subscriber has been started")
+
+    # Print the received tag info to the terminal
+    def match_goal_callback(self, msg):
+        self.get_logger().info('RFID read: "%s"' % msg.data)
+        # Ermittle Koordinaten die mit Ziel der jeweiligen Person übereinstimmen
+        file_content = dict(read_from_file())
+        ## Struktur der Datei: {Name: [x, y]} also [Name][Korordinate x/y ]
+        x = file_content[msg.data][0]
+        y = file_content[msg.data][1]
+        print(str(x) + " " + str(y))
+        response = self.minimal_client.send_request(int(x * 1000), int(y * 1000))
+        self.minimal_client.get_logger().info(
+        'Navigation to  %d + %d completed. answer: %d' %
+        (x, y, response.sum))
+
 class MinimalClientAsync(Node):
 
     def __init__(self):
@@ -19,8 +48,9 @@ class MinimalClientAsync(Node):
         self.cli = self.create_client(AddTwoInts, 'add_two_ints')
         while not self.cli.wait_for_service(timeout_sec=100.0):
             self.get_logger().info('service not available, waiting again...')
-        self.req = AddTwoInts.Request()
+        self.req = AddTwoInts.Request() 
 
+    # gewünschte x und y Koordinaten werden an den Server gesendet
     def send_request(self, a, b):
         self.req.a = a
         self.req.b = b
@@ -31,23 +61,32 @@ class MinimalClientAsync(Node):
 
 def main():
     rclpy.init()
+    # Koordinaten zurücksetzen zum Initalisieren
     x = 0
     y = 0
 
+    name_tag_subscriber = NameTagSubscriber()
     minimal_client = MinimalClientAsync()
     try:
         if sys.argv[3] is not None:
             x = sys.argv[1]
             y = sys.argv[2]
     except IndexError:
+    
+    ## Dies soll in eine Callbakc Funktion des RFID Subscribers ##
+    #   
+        # Ermittle Koordinaten die mit Ziel der jeweiligen Person übereinstimmen
         file_content = dict(read_from_file())
+        ## Struktur der Datei: {Name: [x, y]} also [Name][Korordinate x/y ]
         x = file_content[sys.argv[1]][0]
         y = file_content[sys.argv[1]][1]
         print(str(x) + " " + str(y))
-    response = minimal_client.send_request(int(x * 1000), int(y * 1000))
-    minimal_client.get_logger().info(
+        response = minimal_client.send_request(int(x * 1000), int(y * 1000))
+        minimal_client.get_logger().info(
         'Navigation to  %d + %d completed. answer: %d' %
         (x, y, response.sum))
+    #
+    ## - ##
 
     minimal_client.destroy_node()
     rclpy.shutdown()
